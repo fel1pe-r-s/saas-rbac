c7bf6ae308c601188614a5c01da6450ae21a8606
fel1pe-r-s <1felipersilva1@gmail.com>
2024-06-03 13:12:55 -0300

    middleware de autenticação

--- apps/api/src/http/routes/auth/get-profile.ts
+++ apps/api/src/http/routes/auth/get-profile.ts
@@ -1,49 +1,52 @@
 import type { FastifyInstance } from "fastify";
 import type { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from "zod";
-
 import { prisma } from "@/lib/prisma";
+import { auth } from "@/http/middlewares/auth";
 import { BadRequestError } from "../_errors/bad-request-error";
 
 export async function getProfile(app: FastifyInstance) {
-  app.withTypeProvider<ZodTypeProvider>().get(
-    "/profile",
-    {
-      schema: {
-        tags: ["Auth"],
-        summary: "Get authenticated user profile",
-        response: {
-          200: z.object({
-            user: z.object({
-              id: z.string().uuid(),
-              name: z.string().nullable(),
-              email: z.string().email(),
-              avatarUrl: z.string().url().nullable(),
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      "/profile",
+      {
+        schema: {
+          tags: ["Auth"],
+          summary: "Get authenticated user profile",
+          response: {
+            200: z.object({
+              user: z.object({
+                id: z.string().uuid(),
+                name: z.string().nullable(),
+                email: z.string().email(),
+                avatarUrl: z.string().url().nullable(),
+              }),
             }),
-          }),
+          },
         },
       },
-    },
-    async (request, reply) => {
-      const { sub } = await request.jwtVerify<{ sub: string }>();
+      async (request, reply) => {
+        const { sub } = await request.jwtVerify<{ sub: string }>();
 
-      const user = await prisma.user.findUnique({
-        select: {
-          id: true,
-          name: true,
-          email: true,
-          avatarUrl: true,
-        },
-        where: {
-          id: sub,
-        },
-      });
+        const user = await prisma.user.findUnique({
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            avatarUrl: true,
+          },
+          where: {
+            id: sub,
+          },
+        });
 
-      if (!user) {
-        throw new BadRequestError("User not found.");
+        if (!user) {
+          throw new BadRequestError("User not found.");
+        }
+
+        return reply.send({ user });
       }
-
-      return reply.send({ user });
-    }
-  );
+    );
 }
