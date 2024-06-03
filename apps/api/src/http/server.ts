269839517665efcbad2329792e99b35f69aeba0e
fel1pe-r-s <1felipersilva1@gmail.com>
2024-06-03 14:14:16 -0300

    Listagem de convites

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -34,6 +34,7 @@ import { updateMember } from "./routes/members/update-member";
 import { removeMember } from "./routes/members/remove-member";
 import { createInvite } from "./routes/invites/create-invit";
 import { getInvite } from "./routes/invites/get-invite";
+import { getInvites } from "./routes/invites/get-invites";
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.setSerializerCompiler(serializerCompiler);
@@ -94,6 +95,7 @@ app.register(updateMember);
 app.register(removeMember);
 app.register(createInvite);
 app.register(getInvite);
+app.register(getInvites);
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log("HTTP server running!");
