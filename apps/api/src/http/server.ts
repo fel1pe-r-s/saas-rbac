fc53631fe786578c26072cbae267b47a930795dd
fel1pe-r-s <1felipersilva1@gmail.com>
2024-06-03 14:05:37 -0300

    Atualizar membro

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -30,6 +30,7 @@ import { getProject } from "./routes/projects/get-project";
 import { getProjects } from "./routes/projects/get-projects";
 import { updateProject } from "./routes/projects/update-project";
 import { getMembers } from "./routes/members/get-members";
+import { updateMember } from "./routes/members/update-member";
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.setSerializerCompiler(serializerCompiler);
@@ -86,6 +87,7 @@ app.register(getProject);
 app.register(getProjects);
 app.register(updateProject);
 app.register(getMembers);
+app.register(updateMember);
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log("HTTP server running!");
